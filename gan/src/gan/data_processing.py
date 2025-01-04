940d891093198201ffe7d2cdc958fd13e56ce34d
Anthony Lee <anthony8lee@gmail.com>
2025-01-03 17:18:04 -0700

    Added tanh to rgb range mapper

--- gan/src/gan/data_processing.py
+++ gan/src/gan/data_processing.py
@@ -6,3 +6,9 @@ def normalize_image(image:torch.Tensor):
     image = image.to(dtype=torch.float32)  # In case the image isn't in this dtype
     image = image / (255/2) - 1
     return image
+
+def map_tanh_to_rgb(image:torch.Tensor):
+    """Map from the tanh range to RGB range."""
+    image = image.to(dtype=torch.float32)  # Just in case it is the wrong dtype
+    image = (image+1) * (255/2)
+    return image
