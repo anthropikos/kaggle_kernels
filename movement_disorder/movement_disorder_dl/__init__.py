27c9900d6c1a096b18df57e69490d371a4a51d19
Anthony Lee <anthony8lee@gmail.com>
2025-03-28 13:32:11 -0700

    Created base library logger.

--- movement_disorder/movement_disorder_dl/__init__.py
+++ movement_disorder/movement_disorder_dl/__init__.py
@@ -1,5 +1,10 @@
 # 2025-02-12 Anthony Lee
 
+import logging
+# Logger for library - opt-in approach - https://realpython.com/python-logging-source-code/#library-vs-application-logging-what-is-nullhandler
+formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__).addHandler(logging.NullHandler())
+
 from . import data
 from . import model
 from . import tuner
