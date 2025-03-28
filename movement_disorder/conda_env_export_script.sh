d8e8401066b36446f56842a927e40edea25cdc0c
Anthony Lee <anthony8lee@gmail.com>
2025-03-28 13:54:05 -0600

    Updated

--- movement_disorder/conda_env_export_script.sh
+++ movement_disorder/conda_env_export_script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-conda env export --no-builds | grep -v 'prefix*' > environment.yml
+# With version for posterity
+conda env export > environment_$(hostname).yml
+
+# Without build nor version number - Cross OS compatibility
+conda env export --no-builds | grep -v 'prefix*' | sed 's/=.*$//' > environment.yml
